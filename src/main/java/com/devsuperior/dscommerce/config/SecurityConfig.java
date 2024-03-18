c30571de709f91425a2344956594717b89279ec9
Rian Furtado <rianoliveirafurtado@gmail.com>
2024-03-18 09:59:09 -0300

    Add BCrypt password

--- src/main/java/com/devsuperior/dscommerce/config/SecurityConfig.java
+++ src/main/java/com/devsuperior/dscommerce/config/SecurityConfig.java
@@ -3,8 +3,9 @@ package com.devsuperior.dscommerce.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
 @Configuration
 public class SecurityConfig {
 
@@ -13,6 +14,11 @@ public class SecurityConfig {
         http.csrf(csrf -> csrf.disable());
         http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
         return http.build();
+    }
+
+    @Bean
+    PasswordEncoder getPasswordEnconder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
